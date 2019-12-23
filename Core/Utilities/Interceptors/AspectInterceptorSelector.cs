5be6201acb1e175e29c6ef9131157b71d455b870
Emel Kırımlı <krmlemel@gmail.com>
2019-12-23 15:25:56 +0300

    core-part-2/validation

---
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,23 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+
+namespace Core.Utilities.Interceptors
+{
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodINterceptionBaseAttribute>
+                 (inherit: true).ToList();
+
+            var methodAttributes = type.GetMethod(method.Name).GetCustomAttributes<MethodINterceptionBaseAttribute>(inherit:true);
+            classAttributes.AddRange(methodAttributes);
+
+            return classAttributes.OrderBy(x => x.Priority).ToArray();
+        }
+    }
+}
